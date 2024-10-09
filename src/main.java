fd7876450a544ff003651465cbac5acd20e74a30
jake <ugw2ad@virginia.ede>
2024-10-09 19:06:59 -0400

    Add MyHashSetV1

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList<Integer> num = new LinkedList<>();
+        num.add(1);
+        num.add(1);
+        num.add(1);
+
+
+    }
+}
